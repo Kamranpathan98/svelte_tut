8af3fdf85c40b4562998391c47d2310dbaafa2ab
Kamran <kamranpathan98@gmail.com>
2024-03-13 11:54:51 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	export let name;
+	export let age;
+
+	const number =  1234567890;
+	let count = 0
+	const textString = "The total count is";
+
+	const htmlstring = "<b>This is bold text in html and we can bind it using @html</b>"
+
+	function addNumber() {
+		count += 1
+	}
+</script>
+
+<main>
+	<h1>Hello {name} your age is {age} and the number is {number}!</h1>
+	<button on:click={addNumber}>Add Number</button>
+	<h3>{textString} : {count}</h3>
+	<p>{@html htmlstring}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
